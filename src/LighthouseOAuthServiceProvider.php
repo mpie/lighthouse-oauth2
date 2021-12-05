cea84e9318c312f30a7903f55e3a4648d30b9b41
Mpie <info@mpie.nl>
2021-12-05 03:41:41 +0100

    fix path

--- src/LighthouseOAuthServiceProvider.php
+++ src/LighthouseOAuthServiceProvider.php
@@ -18,11 +18,11 @@ class LighthouseOAuthServiceProvider extends ServiceProvider
     protected function registerConfig()
     {
         $this->publishes([
-            __DIR__.'/../../config/config.php' => config_path('lighthouse-oauth2.php'),
+            __DIR__.'/../config/config.php' => config_path('lighthouse-oauth2.php'),
         ], 'lighthouse-oauth2-config');
 
         $this->publishes([
-            __DIR__.'/../../graphql/oauth2.graphql' => base_path('graphql/oauth2.graphql'),
+            __DIR__.'/../graphql/oauth2.graphql' => base_path('graphql/oauth2.graphql'),
         ], 'lighthouse-oauth2-schema');
     }
 }
