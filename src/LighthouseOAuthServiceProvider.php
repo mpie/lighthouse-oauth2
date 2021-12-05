d34f316fa2520d5d08511df4a8384c1540d07f43
Mpie <info@mpie.nl>
2021-12-05 03:29:57 +0100

    add service provider

---
+++ src/LighthouseOAuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mpie\LighthouseOAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+class LighthouseOAuthServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+        $this->registerConfig();
+    }
+
+    protected function registerConfig()
+    {
+        $this->publishes([
+            __DIR__.'/../../config/config.php' => config_path('lighthouse-oauth2.php'),
+        ], 'config');
+
+        $this->publishes([
+            __DIR__.'/../../graphql/oauth2.graphql' => base_path('graphql/oauth2.graphql'),
+        ], 'schema');
+    }
+}
